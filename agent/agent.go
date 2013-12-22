d02c4f3cfbfb87355bf909929f14d8867191ccc4
Evan Hazlett <ejhazlett@gmail.com>
2013-12-21 23:52:21 -0500

    gofmt

--- agent/agent.go
+++ agent/agent.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-    dockerclient "github.com/fsouza/go-dockerclient"
-    "github.com/dotcloud/docker"
-    _ "github.com/shipyard/shipyard-go/shipyard"
-    "fmt"
-    "os"
-    "io"
-    "flag"
-    "time"
-    "io/ioutil"
-    "encoding/json"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "strings"
-    "strconv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/dotcloud/docker"
+	dockerclient "github.com/fsouza/go-dockerclient"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var DockerURL = flag.String("host", "http://127.0.0.1:4243", "Docker URL")
@@ -26,93 +25,97 @@ var Register = flag.Bool("register", false, "Register Agent with Shipyard")
 var Port = flag.Int("port", 4500, "Agent Listen Port")
 
 type AgentData struct {
-    Key     string `json:"key"`
+	Key string `json:"key"`
 }
 type ContainerData struct {
-    Container   docker.APIContainers
-    Meta        *docker.Container
+	Container docker.APIContainers
+	Meta      *docker.Container
 }
 
 func doPostRequest(path string, body io.Reader) ([]byte, int) {
-    client := &http.Client{}
-    appUrl := fmt.Sprintf("%v%v", *ShipyardURL, path)
-    req, _ := http.NewRequest("POST", appUrl, body)
-    req.Header.Set("Authorization", fmt.Sprintf("AgentKey:%v", *ShipyardKey))
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Println("Error sending containers to Shipyard: ", err)
-        return nil, 500
-    }
-    defer resp.Body.Close()
-    b, _ := ioutil.ReadAll(resp.Body)
-    return b, resp.StatusCode
+	client := &http.Client{}
+	appUrl := fmt.Sprintf("%v%v", *ShipyardURL, path)
+	req, _ := http.NewRequest("POST", appUrl, body)
+	req.Header.Set("Authorization", fmt.Sprintf("AgentKey:%v", *ShipyardKey))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending containers to Shipyard: ", err)
+		return nil, 500
+	}
+	defer resp.Body.Close()
+	b, _ := ioutil.ReadAll(resp.Body)
+	return b, resp.StatusCode
 }
 
 func pushContainers() {
-    client, err := dockerclient.NewClient(*DockerURL)
-    if err != nil { panic(err) }
-    opts := dockerclient.ListContainersOptions{All: true}
-    containers, _ := client.ListContainers(opts)
-    data := make([]ContainerData, len(containers))
-    for x, c := range(containers) {
-        i,_ := client.InspectContainer(c.ID)
-        containerData := ContainerData{Container: c, Meta: i}
-        data[x] = containerData
-    }
-    cnt, _ := json.Marshal(data)
-    containerJSON := string(cnt)
-    doPostRequest("/agent/containers/", strings.NewReader(containerJSON))
+	client, err := dockerclient.NewClient(*DockerURL)
+	if err != nil {
+		panic(err)
+	}
+	opts := dockerclient.ListContainersOptions{All: true}
+	containers, _ := client.ListContainers(opts)
+	data := make([]ContainerData, len(containers))
+	for x, c := range containers {
+		i, _ := client.InspectContainer(c.ID)
+		containerData := ContainerData{Container: c, Meta: i}
+		data[x] = containerData
+	}
+	cnt, _ := json.Marshal(data)
+	containerJSON := string(cnt)
+	doPostRequest("/agent/containers/", strings.NewReader(containerJSON))
 }
 
 func pushImages() {
-    client, err := dockerclient.NewClient(*DockerURL)
-    if err != nil { panic(err) }
-    images, _ := client.ListImages(false)
-    imageData, _ := json.Marshal(images)
-    imageJSON := string(imageData)
-    doPostRequest("/agent/images/", strings.NewReader(imageJSON))
+	client, err := dockerclient.NewClient(*DockerURL)
+	if err != nil {
+		panic(err)
+	}
+	images, _ := client.ListImages(false)
+	imageData, _ := json.Marshal(images)
+	imageJSON := string(imageData)
+	doPostRequest("/agent/images/", strings.NewReader(imageJSON))
 }
 
 func listen(d time.Duration, f func(time.Time)) {
-    for {
-        f(time.Now())
-        time.Sleep(d)
-    }
+	for {
+		f(time.Now())
+		time.Sleep(d)
+	}
 }
 
 // Registers with Shipyard at the specified URL
 func register() {
-    fmt.Println("Registering with ", *ShipyardURL)
-    registerURL := fmt.Sprintf("%v/agent/register/", *ShipyardURL)
-    hostname, _ := os.Hostname()
-    vals := url.Values{"name": {hostname}, "port": {strconv.Itoa(*Port)}}
-    resp, _ := http.PostForm(registerURL, vals)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    var data AgentData
-    json.Unmarshal(body, &data)
-    fmt.Println("Agent Key: ", data.Key)
+	fmt.Println("Registering with ", *ShipyardURL)
+	registerURL := fmt.Sprintf("%v/agent/register/", *ShipyardURL)
+	hostname, _ := os.Hostname()
+	vals := url.Values{"name": {hostname}, "port": {strconv.Itoa(*Port)}}
+	resp, _ := http.PostForm(registerURL, vals)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var data AgentData
+	json.Unmarshal(body, &data)
+	fmt.Println("Agent Key: ", data.Key)
 }
 
 func run(t time.Time) {
-    go pushContainers()
-    go pushImages()
-}   
+	go pushContainers()
+	go pushImages()
+}
 
 func main() {
-    flag.Parse()
-    if *ShipyardURL == "" {
-        fmt.Println("Error: You must specify a Shipyard URL")
-        os.Exit(1)
-    }
-    duration, _ := time.ParseDuration(fmt.Sprintf("%vs", *RunInterval))
-    if *Register {
-        register()
-    } else {
-        fmt.Println("Shipyard Agent", fmt.Sprintf(" (%s)", *ShipyardURL))
-        u, _ := url.Parse(*DockerURL)
-        proxy := httputil.NewSingleHostReverseProxy(u)
-        go http.ListenAndServe(fmt.Sprintf(":%v", *Port), proxy)
-        listen(duration, run)
-    }
+	flag.Parse()
+	if *ShipyardURL == "" {
+		fmt.Println("Error: You must specify a Shipyard URL")
+		os.Exit(1)
+	}
+	duration, _ := time.ParseDuration(fmt.Sprintf("%vs", *RunInterval))
+	if *Register {
+		register()
+	} else {
+		fmt.Println("Shipyard Agent", fmt.Sprintf(" (%s)", *ShipyardURL))
+		u, _ := url.Parse(*DockerURL)
+		proxy := httputil.NewSingleHostReverseProxy(u)
+		go http.ListenAndServe(fmt.Sprintf(":%v", *Port), proxy)
+		listen(duration, run)
+	}
 }
